641246701fe5cccd4794b151aec6d9ce5c8f14c2
Daniel Shin <kyuds1016@berkeley.edu>
2023-02-03 15:50:53 -0800

    clean up boot script

--- rs_script/boot.sh
+++ rs_script/boot.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 mode="$1"
 head_ip="$2"
-port="$3"
+listener="$3"
+port="$4"
+
+if [[ ! listener ]]; then
+    listener="7001"
+fi
 
 if [[ ! $port ]]; then
     port="6379"
 fi
 
 if [ "$mode" = "head" ]; then
-    ray start --head --port=$port --ray-client-server-port=7001 --node-ip-address=$(ipconfig getifaddr en0)
+    ip=""
     # get ips
     if [[ "$OSTYPE" = "darwin"* ]]; then # macOS
         ip=$(ipconfig getifaddr en0)
         echo "Started head node on: $ip"
     # TODO: add linux configs to find IP.
     fi
+
+    # start ray 
+    ray start --head --port=$port --ray-client-server-port=$listener --node-ip-address=$ip
 elif [ "$mode" = "worker" ]; then
     adrs="$head_ip:$port"
     echo "Connecting worker to: $adrs"
