0ac13705e0f85830f489ed8a31e2c6ba3e27f8f6
Tony <tshong@berkeley.edu>
2023-02-13 13:16:57 -0800

    misc changes

--- rs_script/spill_test.py
+++ rs_script/spill_test.py
@@ -1,12 +1,14 @@
 import ray
 import numpy as np
 import os
+from sys import getsizeof
 
 os.environ["RAY_BACKEND_LOG_LEVEL"] = "debug"
 
 @ray.remote
 def huge():
     x = np.zeros((10 ** 9, 2))
+    print(getsizeof(x))
     return x
 
 ray.get(huge.remote())
