247b072b2fffa1d051f1b617e017aca32e06af5f
Tony Hong <tonyhong007@gmail.com>
2023-04-18 11:07:41 +0000

    debug

--- src/ray/object_manager/object_manager.cc
+++ src/ray/object_manager/object_manager.cc
@@ -1124,7 +1124,7 @@ bool RemoteSpill::RemoteSpillReceiveObjectChunk(const NodeID &node_id,
   // doesn't care about fault tolerance. 
   if (from_remote_spill) {
     if (!received_remote_objects_origin_.contains(object_id)) {
-      RAY_LOGO(INFO) << "Increasing ref count of about for remote spill";
+      RAY_LOG(INFO) << "Increasing ref count of about for remote spill";
       received_remote_objects_origin_.emplace(object_id, node_id);
       buffer_pool_store_client_->RemoteSpillIncreaseObjectCount(object_id);
     }
